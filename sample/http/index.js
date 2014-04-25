6397e4fef8f8466a6337b65b5462d40148131c10
Leo Dion <leogdion@brightdigit.com>
2014-04-25 10:48:05 -0400

    working on request

--- sample/http/index.js
+++ sample/http/index.js
@@ -5,3 +5,5 @@ var rql = restquel(__dirname + "/../configuration.json");
 var server = http.createServer(rql);
 
 rql.ready(server, 8080);
+
+http.get("http://localhost:8080/api/v1/items", function (res) {});
